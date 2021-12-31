8c4c60f5f65908cd26a7ec3b83537030a02cd372
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2021-12-30 21:12:17 -0500

    Add files via upload

--- pages/eom.js
+++ pages/eom.js
@@ -1,27 +1,39 @@
-import { Toolbar } from '../components/toolbar';
 import styles from '../styles/EOM.module.css';
+import { Toolbar } from '../components/toolbar';
 
-export const EOM = ({  }) => {
-   
-    return(<div className="page-container"> 
-    <Toolbar/>
-        <div className={styles.main}> 
-            <h1> Employee Of The Month</h1>
 
-            <div className={styles.employeeOfTheMonth}> 
-                <h3> Kevin Le</h3>
-                <h6> Aspiring Software Developer</h6>
-                <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHAxPcKLg9bqg/profile-displayphoto-shrink_800_800/0/1608509536258?e=1646265600&v=beta&t=pmqpEtfOQ2BqRmQlb8tDOHAHVSAaw0naB5IqU7Fqn8w"/>
-                <p>"Sometimes in life it takes the longest time to say the simplest things."</p>
+export const EOM = ({ employee }) => {
+    console.log(employee);
+    return (
+        <div className='page-container'>
+                <Toolbar />
 
+            <div className={styles.main}>
+                <h1>Employee Of The Month</h1>
+
+                <div className = {styles.employeeOfTheMonth}>
+                    <h3>{employee.name}</h3>
+                    <h6>{employee.position}</h6>
+                    <img src={employee.image} />
+                    <p>{employee.description}</p>
+                </div>
             </div>
-
-
         </div>
-    
-    </div>)
+    );
 };
 
+export const getServerSideProps = async pageContext => {
+    const apiResponse = await fetch(
+        'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth',
+    );
 
+    const employee = await apiResponse.json();
+
+    return {
+        props: {
+            employee
+        }
+    }
+};
 
 export default EOM;
